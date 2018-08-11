4788426930781ef96400afdf42f1a675f2eb74d7
Eric Klimowich <eklimowich@hotmail.com>
2018-08-11 07:34:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,8 +10,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
+def move(board, index, current_player)
+  board[index.to_i - 1] = current_player
 end
 
 def valid_move?(board, index)
