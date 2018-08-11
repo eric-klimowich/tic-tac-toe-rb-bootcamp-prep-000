6404d1fbfddef184f1c8fdd151373027f9ce2e50
Eric Klimowich <eklimowich@hotmail.com>
2018-08-11 07:11:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,13 +95,5 @@ WIN_COMBINATIONS = [
   end
   
   def play(board)
-  until over?(board)
-    current_player(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  else
-    puts "Cat's Game!"
-  end
+  input = gets 
 end
