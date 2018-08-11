bbb8a4bf6536a016b71040f40b7b2936a94f7e93
Eric Klimowich <eklimowich@hotmail.com>
2018-08-11 06:59:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,3 +93,15 @@ WIN_COMBINATIONS = [
       nil
     end
   end
+  
+  def play(board)
+  until over?(board)
+    current_player(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else
+    puts "Cats Game!"
+  end
+end
