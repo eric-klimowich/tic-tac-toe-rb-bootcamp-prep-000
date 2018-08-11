bf5a83bc69ccb19f2ec9d469f1b16b00d9ea3e6f
Eric Klimowich <eklimowich@hotmail.com>
2018-08-11 07:35:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player)
-  board[index.to_i] = current_player
+  board[index] = current_player
 end
 
 def valid_move?(board, index)
